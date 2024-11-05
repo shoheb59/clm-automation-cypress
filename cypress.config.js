91cacb0e0f863c3823ee05d7d7d697adca1f41d7
Md. Hasnat Shoheb <hasnat.shoheb@selise.ch>
2024-11-05 18:25:50 +0600

    Allure Report

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,20 @@
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
-const { allureCypress } = require ("allure-cypress/reporter");
+const { allureCypress } = require("allure-cypress/reporter");
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   reporter: 'mochawesome',
-  watchForFileChanges: false, 
+  watchForFileChanges: false,
 
-  
   e2e: {
-    defaultCommandTimeout: 10000, 
+    defaultCommandTimeout: 10000,
     pageLoadTimeout: 30000,
     setupNodeEvents(on, config) {
       allureWriter(on, config);
-      return config;
+      on('task', {
+        reportAllureCypressSpecMessages: allureCypress.reportAllureCypressSpecMessages, // Corrected the function reference
+        writeAllureResults: allureWriter.writeAllureResults // Make sure this task is also registered if needed
+      });
     },
   },
   env: {
